e9794c08610272ed78e7e7247882b9a706242e23
Kyle <kylejmbaylis@gmail.com>
2022-04-12 19:42:11 -0400

    learning ES6 syntax

---
+++ app.js
@@ -0,0 +1,18 @@
+const commandLineArgs = process.argv.slice(2, process.argv.length);
+
+// console.log(commandLineArgs);
+
+const printProfileData = profileDataArr => {
+
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("==========");
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(commandLineArgs);
