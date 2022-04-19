f1c5af7d112a2190229345c4d8a7494ccd959b46
Kyle <kylejmbaylis@gmail.com>
2022-04-19 19:25:13 -0400

    inquirer install and first test

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
+const inquirer = require('inquirer');
+/*
 const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
 
-// Old way
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
-
-// New way
-const [name, github] = profileDataArgs;
-
-// console.log(commandLineArgs);
-/* From lesson 1
-const printProfileData = profileDataArr => {
-
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==========');
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs); */
-
-// Lesson 2
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
