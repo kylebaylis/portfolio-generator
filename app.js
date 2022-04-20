a70ad5fa7c006c182d1abb00bb2a76b9dc623893
Kyle <kylejmbaylis@gmail.com>
2022-04-19 20:09:26 -0400

    added validation

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
