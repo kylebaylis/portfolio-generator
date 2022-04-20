8da9519c7dcf23bae8892899a285e684a0344594
Kyle <kylejmbaylis@gmail.com>
2022-04-19 20:42:04 -0400

    generate html page

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const inquirer = require('inquirer');
-/*
+
 const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
 
-const pageHTML = generatePage(name, github);
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
-*/
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -114,5 +105,10 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-    });
+        const pageHTML = generatePage(portfolioData);
+    
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+          
+        });
+      });
